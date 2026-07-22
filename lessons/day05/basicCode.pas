c5256d0e18c1a4f0f98e6593f1e2be568ee5b24d
Folombas <folombas@yandex.ru>
2026-07-22 03:13:10 +0300

    Встроенные функции ord и chr в Free Pascal

---
+++ lessons/day05/basicCode.pas
@@ -0,0 +1,31 @@
+program basicCode
+
+var:
+	c: char;
+	n: integer;
+	s: string;
+
+begin
+	// Символ -> код
+	Write('Введите символ: ');
+	Readln(s);
+	if s = '' then
+	begin
+		Writeln('Символ не введён.');
+		Halt;
+	end;
+
+	c := s[1];
+	n := Ord(c);
+	Writeln('Код символа ', c, ' = ', n);
+	
+	Writeln;
+	
+	// Код -> символ
+	Write('Введите код символа: ');
+	Readln(c);
+	
+	// chr работает для диапазона символов; для обычного ASII обычно достаточно 0...255
+	c := Chr(n);
+	Writeln('Символ с кодом ', n, ' = ', c);
+end.
