94a61ef6939f85ff34846c50d50ce2fca873e350
Folombas <folombas@yandex.ru>
2026-09-04 01:40:05 +0300

    Исправлен ввод: добавлена очистка буфера после каждого символа

--- symbolTypeExtended.pas
+++ symbolTypeExtended.pas
@@ -1,20 +1,22 @@
 program SymbolTypeExtended;
 var
-	c: char;
-	code: integer;
+    s: string;
+    i: integer;
 begin
-	writeln('Вводите символы (для завершения введите точку):');
-	repeat
-		read(c);
-		code := ord(c);
-		write('Символ ''', c, ''' (код ', code, ') - ');
-		case c of
-			'A'..'Z': writeln('заглавная латинская буква');
-			'a'..'z': writeln('строчная латинская буква');
-			'0'..'9': writeln('цифра');
-		else
-			writeln('неопознанный символ');
-		end;
-	until c = '.';
-	writeln('Программа завершена.');
+    writeln('Введите строку для анализа:');
+    readln(s);
+    for i := 1 to length(s) do
+    begin
+        c := s[i];
+        code := ord(c);
+        write('Символ ''', c, ''' (код ', code, ') - ');
+        case c of
+            'A'..'Z': writeln('заглавная латинская буква');
+            'a'..'z': writeln('строчная латинская буква');
+            '0'..'9': writeln('цифра');
+        else
+            writeln('неопознанный символ');
+        end;
+    end;
+    writeln('Программа завершена.');
 end.
