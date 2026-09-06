a7327282a9da2f03b12b8e6c458e1ecf6a8ae6a2
Folombas <folombas@yandex.ru>
2026-09-06 23:27:27 +0300

    добавили в программу MovingHello выбор цвета текста с палитрой 1–15, сохранением цвета при перемещении стрелками и выходом по любой обычной клавише.

--- movehello.pas
+++ movehello.pas
@@ -1,95 +1,116 @@
 program MovingHello;
 uses crt;
+
 const
-	KeyLeft = -75;
-	KeyRight = -77;
-	KeyUp = -72;
-	KeyDown = -80;
-	TheMessage = 'Hello, World of Free Pascal';
-	
+    KeyLeft     = -75;
+    KeyRight    = -77;
+    KeyUp       = -72;
+    KeyDown     = -80;
+    TheMessage  = 'Hello, World of Free Pascal';
+
 procedure GetKey(var code: integer);
 var
-	c: char;
+    c: char;
 begin
-	c := ReadKey;
-	if c = #0 then
-	begin
-		c := ReadKey;
-		code := -ord(c)
-	end
-	else
-	begin
-		code := ord(c)
-	end
+    c := ReadKey;
+    if c = #0 then
+    begin
+        c := ReadKey;
+        code := -ord(c)
+    end
+    else
+    begin
+        code := ord(c)
+    end
 end;
 
-procedure ShowMessage(x, y: integer; msg: string);
+procedure ShowMessage(x, y: integer; msg: string; color: byte);
 begin
-	GotoXY(x, y);
-	write(msg);
-	GotoXY(1, 1)
+    TextColor(color);
+    GotoXY(x, y);
+    write(msg);
+    GotoXY(1, 1)
 end;
 
 procedure HideMessage(x, y: integer; msg: string);
 var
-	len, i: integer;
+    len, i: integer;
 begin
-	len := length(msg);
-	GotoXY(x, y);
-	for i := 1 to len do
-		write(' ');
-	GotoXY(1, 1)
+    len := length(msg);
+    GotoXY(x, y);
+    for i := 1 to len do
+        write(' ');
+    GotoXY(1, 1)
 end;
 
-procedure MoveMessage(var x, y: integer; msg: string; dx, dy: integer);
+procedure MoveMessage(var x, y: integer; msg: string; dx, dy: integer; color: byte);
 begin
-	HideMessage(x, y, msg);
-	x := x + dx;
-	y := y + dy;
-	ShowMessage(x, y, msg)
+    HideMessage(x, y, msg);
+    x := x + dx;
+    y := y + dy;
+    ShowMessage(x, y, msg, color)
+end;
+
+procedure ShowColorPalette;
+var
+    i: integer;
+begin
+    clrscr;
+    writeln('Выберите цвет текста (введите номер от 1 до 15):');
+    writeln;
+    for i := 1 to 15 do
+    begin
+        TextColor(i);
+        write(i:3, ' ');
+    end;
+    writeln;
+    writeln;
+    TextColor(7);  { возвращаем стандартный цвет }
+    writeln('Ваш выбор: ');
+end;
+
+function SelectColor: byte;
+var
+    color: integer;
+begin
+    repeat
+        ShowColorPalette;
+        readln(color);
+    until (color >= 1) and (color <= 15);
+    SelectColor := color;
+    clrscr
 end;
 
 var
-	CurX, CurY: integer;
-	c: integer;
+    CurX, CurY: integer;
+    c: integer;
+    msgColor: byte;
 begin
-	clrscr;
-	CurX := (ScreenWidth - length(TheMessage)) div 2;
-	CurY := ScreenHeight div 2;
-	ShowMessage(CurX, CurY, TheMessage);
-	while true do
-	begin
-		GetKey(c);
-		if c > 0 then        { non-extended code; quit }
-			break;
-		case c of
-			KeyLeft:
-				MoveMessage(CurX, CurY, TheMessage, -1, 0);
-			KeyRight:
-				MoveMessage(CurX, CurY, TheMessage, 1, 0);
-			KeyUp:
-				MoveMessage(CurX, CurY, TheMessage, 0, -1);
-			KeyDown:
-				MoveMessage(CurX, CurY, TheMessage, 0, 1)
-		end
-	end;
-	clrscr
+    clrscr;
+    msgColor := SelectColor;
+    
+    CurX := (ScreenWidth - length(TheMessage)) div 2;
+    CurY := ScreenHeight div 2;
+    ShowMessage(CurX, CurY, TheMessage, msgColor);
+    
+    while true do
+    begin
+        GetKey(c);
+        if c > 0 then        { non-extended code; quit }
+            break;
+        case c of
+            KeyLeft:
+                MoveMessage(CurX, CurY, TheMessage, -1, 0, msgColor);
+            KeyRight:
+                MoveMessage(CurX, CurY, TheMessage, 1, 0, msgColor);
+            KeyUp:
+                MoveMessage(CurX, CurY, TheMessage, 0, -1, msgColor);
+            KeyDown:
+                MoveMessage(CurX, CurY, TheMessage, 0, 1, msgColor)
+        end
+    end;
+    clrscr
 end.
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
 			
 			
 			
