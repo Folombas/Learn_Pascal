07f5cb2fbe9a171ab21d714de8094c7b0da3af40
Folombas <folombas@yandex.ru>
2026-09-08 23:57:07 +0300

    Добавить цветную рамку вокруг сообщения и случайный цвет текста

--- HelloCrt.pas
+++ HelloCrt.pas
@@ -2,17 +2,41 @@ program HelloCrt;
 uses crt;
 const
   TheMessage = 'Hello, World Programming of Free Pascal';
-  DelayDuration = 3000; { 3 seconds }
 var
-  x, y: integer;
+  x, y, i: integer;
+  color: byte;
 begin
   clrscr;
-  TextColor(Red);          { устанавливаем красный цвет текста }
+  randomize;
+  color := random(15) + 1;          { случайный цвет от 1 до 15 }
+  TextColor(color);
+
   x := (ScreenWidth - length(TheMessage)) div 2;
   y := ScreenHeight div 2;
+
+  { Рисуем рамку вокруг сообщения }
+  GotoXY(x - 2, y - 1);
+  write('+');
+  for i := 1 to length(TheMessage) + 2 do
+    write('-');
+  write('+');
+
+  GotoXY(x - 2, y);
+  write('|');
+  GotoXY(x + length(TheMessage) + 1, y);
+  write('|');
+
+  GotoXY(x - 2, y + 1);
+  write('+');
+  for i := 1 to length(TheMessage) + 2 do
+    write('-');
+  write('+');
+
+  { Выводим само сообщение }
   GotoXY(x, y);
   write(TheMessage);
+
   GotoXY(1, 1);
-  delay(DelayDuration);
+  ReadKey;          { ожидание нажатия любой клавиши }
   clrscr
 end.
