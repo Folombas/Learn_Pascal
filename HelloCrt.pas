5c049fb8c8ae5c15a4663d72ccd9a7943e625dbc
Folombas <folombas@yandex.ru>
2026-09-08 23:59:27 +0300

    Добавить радужную анимацию текста с циклической сменой цветов

--- HelloCrt.pas
+++ HelloCrt.pas
@@ -3,40 +3,26 @@ uses crt;
 const
   TheMessage = 'Hello, World Programming of Free Pascal';
 var
-  x, y, i: integer;
+  x, y: integer;
   color: byte;
 begin
   clrscr;
-  randomize;
-  color := random(15) + 1;          { случайный цвет от 1 до 15 }
-  TextColor(color);
-
   x := (ScreenWidth - length(TheMessage)) div 2;
   y := ScreenHeight div 2;
 
-  { Рисуем рамку вокруг сообщения }
-  GotoXY(x - 2, y - 1);
-  write('+');
-  for i := 1 to length(TheMessage) + 2 do
-    write('-');
-  write('+');
+  color := 1;
+  while not KeyPressed do
+  begin
+    TextColor(color);
+    GotoXY(x, y);
+    write(TheMessage);
+    GotoXY(1, 1);
+    delay(200);
+    Inc(color);
+    if color > 15 then
+      color := 1;
+  end;
 
-  GotoXY(x - 2, y);
-  write('|');
-  GotoXY(x + length(TheMessage) + 1, y);
-  write('|');
-
-  GotoXY(x - 2, y + 1);
-  write('+');
-  for i := 1 to length(TheMessage) + 2 do
-    write('-');
-  write('+');
-
-  { Выводим само сообщение }
-  GotoXY(x, y);
-  write(TheMessage);
-
-  GotoXY(1, 1);
-  ReadKey;          { ожидание нажатия любой клавиши }
+  ReadKey;   { очистка буфера клавиш }
   clrscr
 end.
