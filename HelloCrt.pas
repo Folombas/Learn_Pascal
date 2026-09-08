c916e1b922f4ea7e3848927bb186c1529061b724
Folombas <folombas@yandex.ru>
2026-09-08 23:55:26 +0300

    Изменили цвет текста на красный и сократить задержку до 3 секунд

--- HelloCrt.pas
+++ HelloCrt.pas
@@ -1,17 +1,18 @@
 program HelloCrt;
 uses crt;
 const
-	TheMessage = 'Hello, World Programming of Free Pascal';
-	DelayDuration = 5000; { 3 seconds }
+  TheMessage = 'Hello, World Programming of Free Pascal';
+  DelayDuration = 3000; { 3 seconds }
 var
-	x, y: integer;
+  x, y: integer;
 begin
-	clrscr;
-	x := (ScreenWidth - length(TheMessage)) div 2;
-	y := ScreenHeight div 2;
-	GotoXY(x, y);
-	write(TheMessage);
-	GotoXY(1, 1);
-	delay(DelayDuration);
-	clrscr
+  clrscr;
+  TextColor(Red);          { устанавливаем красный цвет текста }
+  x := (ScreenWidth - length(TheMessage)) div 2;
+  y := ScreenHeight div 2;
+  GotoXY(x, y);
+  write(TheMessage);
+  GotoXY(1, 1);
+  delay(DelayDuration);
+  clrscr
 end.
