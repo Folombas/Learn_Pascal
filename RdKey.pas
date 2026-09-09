3c219e89cfdcb5d1340ba5de9a7474ca35e58cfa
Folombas <folombas@yandex.ru>
2026-09-09 23:31:28 +0300

    Добавили обработку расширенных клавиш с выводом скан-кодов

--- RdKey.pas
+++ RdKey.pas
@@ -1,13 +1,31 @@
 program RdKey;
 uses crt;
+
+procedure GetKey(var code: integer);
 var
-	c, cc: char;
+  c: char;
 begin
-	repeat
-		c := ReadKey;
-		cc := c;
-		if (cc < #32) or (cc > #126) then
-			cc := '?';
-		writeln(ord(c), ' (', cc, ')')
-	until c = ' '
+  c := ReadKey;
+  if c = #0 then
+  begin
+    c := ReadKey;
+    code := -ord(c)   { расширенный код – отрицательный }
+  end
+  else
+    code := ord(c)
+end;
+
+var
+  code: integer;
+begin
+  repeat
+    GetKey(code);
+    if code < 0 then
+      writeln('Extended: ', -code)   { выводим скан-код без минуса }
+    else
+      if (code >= 32) and (code <= 126) then
+        writeln(code, ' (', chr(code), ')')
+      else
+        writeln(code, ' (?)')
+  until code = ord(' ')   { выход по пробелу }
 end.
