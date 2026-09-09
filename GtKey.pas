6b68f891e2689348ac4ee3b30a3a26a8dd5bb7f7
Folombas <folombas@yandex.ru>
2026-09-09 23:35:50 +0300

    Добавили вывод названий для расширенных клавиш и отображение символов для обычных клавиш

--- GtKey.pas
+++ GtKey.pas
@@ -3,25 +3,56 @@ uses crt;
 
 procedure GetKey(var code: integer);
 var
-	c: char;
+  c: char;
 begin
-	c := ReadKey;
-	if c = #0 then
-	begin
-		c := ReadKey;
-		code := -ord(c)
-	end
-	else
-	begin
-		code := ord(c)
-	end
+  c := ReadKey;
+  if c = #0 then
+  begin
+    c := ReadKey;
+    code := -ord(c)
+  end
+  else
+    code := ord(c)
+end;
+
+function GetKeyName(code: integer): string;
+begin
+  case code of
+    -59:  GetKeyName := 'F1';
+    -60:  GetKeyName := 'F2';
+    -61:  GetKeyName := 'F3';
+    -62:  GetKeyName := 'F4';
+    -63:  GetKeyName := 'F5';
+    -64:  GetKeyName := 'F6';
+    -65:  GetKeyName := 'F7';
+    -66:  GetKeyName := 'F8';
+    -67:  GetKeyName := 'F9';
+    -68:  GetKeyName := 'F10';
+    -87:  GetKeyName := 'F11';
+    -88:  GetKeyName := 'F12';
+    -71:  GetKeyName := 'Home';
+    -72:  GetKeyName := 'Up';
+    -73:  GetKeyName := 'PageUp';
+    -75:  GetKeyName := 'Left';
+    -77:  GetKeyName := 'Right';
+    -79:  GetKeyName := 'End';
+    -80:  GetKeyName := 'Down';
+    -81:  GetKeyName := 'PageDown';
+    -82:  GetKeyName := 'Insert';
+    -83:  GetKeyName := 'Delete';
+    else
+      GetKeyName := '';
+  end
 end;
 
 var
-	i: integer;
+  i: integer;
 begin
-	repeat
-		GetKey(i);
-		writeln(i)
-	until i = ord(' ')
+  repeat
+    GetKey(i);
+    if i < 0 then
+      writeln(i:4, ' ', GetKeyName(i))
+    else
+      writeln(i:4, ' (', chr(i), ')')
+  until i = ord(' ')
 end.
