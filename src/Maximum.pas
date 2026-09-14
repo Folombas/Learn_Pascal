307c2ca8d197817077e1772268a73f5b18c2eb0e
Folombas <folombas@yandex.ru>
2026-09-14 23:46:38 +0300

    Простая программа, которая находит большее число из двух

---
+++ src/Maximum.pas
@@ -0,0 +1,14 @@
+program Maximum;
+
+var
+	a, b, M: integer;
+
+begin
+	writeln('Введите два целых числа');
+	read(a, b);
+	if a > b then
+		M := a
+	else
+		M := b;
+	writeln('Наибольшее число ', M);
+end.
