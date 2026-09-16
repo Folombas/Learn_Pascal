37927b6f034d9720da0051983d311514425688e7
Folombas <folombas@yandex.ru>
2026-09-16 23:13:40 +0300

    Добавлена программа max4, которая запрашивает четыре целых числа, последовательно сравнивает их и выводит наибольшее

---
+++ max4.pas
@@ -0,0 +1,17 @@
+program max4;
+
+var
+ a,b,c,d,M:integer;
+ 
+begin
+	writeln('Введите четыре целых числа: ');
+	read(a,b,c,d);
+	M:=a;
+	if b>M then
+		M:=b;
+	if c>M then
+		M:=c;
+	if d>M then
+		M:=d;
+	writeln('Наибольшее число', M);
+end.
