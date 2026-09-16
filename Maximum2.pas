fb8fc1e7c0c61800d8831b7d9d2d0481b748fe97
Folombas <folombas@yandex.ru>
2026-09-16 22:15:24 +0300

    Добавлена программа Maximum2, которая запрашивает два целых числа, сравнивает их и выводит наибольшее

---
+++ Maximum2.pas
@@ -0,0 +1,12 @@
+program Maximum2;
+
+var a, b, M: integer;
+
+begin
+	writeln('Введите два целых числа');
+	read(a, b);
+	M := a;
+	if b > a then
+		M := b;
+	writeln('Наибольшее число ', M);
+end.
