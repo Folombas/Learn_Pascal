82acdbdd2783618bf29228fd51462763fc471551
Folombas <folombas@yandex.ru>
2026-09-17 22:07:44 +0300

    Программа MetroLateEvening

---
+++ MetroLateEvening.pas
@@ -0,0 +1,9 @@
+program MetroLateEvening;
+
+begin
+  writeln('Late evening in the metro.');
+  writeln('The train goes through the tunnel.');
+  writeln('I am in the carriage, writing code.');
+  writeln('This program was created together with AI.');
+  writeln('Almost home.');
+end.
